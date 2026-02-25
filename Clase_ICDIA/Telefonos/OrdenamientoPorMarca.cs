eb63168b0197d0b399b1bceb1e4b7d5f3e6b90df
Oscar Alfredo Pérez Lara <oscarinlara87@gmail.com>
2026-02-25 04:27:55 +0000

    Actualizacion

--- Clase_ICDIA/Telefonos/OrdenamientoPorMarca.cs
+++ Clase_ICDIA/Telefonos/OrdenamientoPorMarca.cs
@@ -5,8 +5,8 @@ public class OrdenamientoPorMarca : IComparer<Shared.Telefonos>
 {
     public int Compare(Shared.Telefonos? x, Shared.Telefonos? y)
     {
-        //int v = x.Marca.CompareTo(y.Marca); // orden ascendente
-        int v = x.Marca.CompareTo(y.Marca) * -1; // orden descendente
+        int v = x.Marca.CompareTo(y.Marca); // orden ascendente
+        //int v = x.Marca.CompareTo(y.Marca) * -1; // orden descendente
         return v;
     }
 }
--- Clase_ICDIA/Telefonos/OrdenamientoPorModelo.cs
+++ Clase_ICDIA/Telefonos/OrdenamientoPorModelo.cs
@@ -5,8 +5,8 @@ public class OrdenamientoPorModelo : IComparer<Shared.Telefonos>
 {
     public int Compare(Shared.Telefonos? x, Shared.Telefonos? y)
     {
-        //int v = x.Modelo.CompareTo(y.Modelo); // orden ascendente
-        int v = x.Modelo.CompareTo(y.Modelo) * -1; // orden descendente
+        int v = x.Modelo.CompareTo(y.Modelo); // orden ascendente
+        //int v = x.Modelo.CompareTo(y.Modelo) * -1; // orden descendente
         return v;
     }
 }
