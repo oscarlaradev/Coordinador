82d5e1267bd36d47ff2aba436025d9778ef2a5d4
Oscar Alfredo Pérez Lara <oscarinlara87@gmail.com>
2026-02-27 05:05:10 +0000

    Actualizacion Basico

--- Clase_ICDIA/Primeros_Programas/Program.cs
+++ Clase_ICDIA/Primeros_Programas/Program.cs
@@ -7,7 +7,13 @@ public class Program
         Console.WriteLine("Programa Primeros_Programas iniciado correctamente.");
 
         //Program programa = new Program();
-        programa1 programa = new programa1();
+        //programa1 programa = new programa1();
+        //programa2 programa2 = new programa2();
+        //programa3 programa3 = new programa3();
+        //programa4 programa4 = new programa4();
+       // programa5 programa5 = new programa5();
+        programa6 programa6 = new programa6();
+       // Alumno alumno = new Alumno();
         
 
     }
